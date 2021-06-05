b6ca1b0bc933255769c56c18e9e0f03cdc04a5ad
kishoreHis <84919348+kishoreHis@users.noreply.github.com>
2021-06-05 18:43:46 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import  Header from  "./Header";
+import  Footer from  "./Footer";
+import  Note  from "./Note";
+function app() {
+return (
+<div>
+       <Header />
+       <Footer />
+       <Note   />
+ </div>
+ );
+}
+export default app;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+
+  import React from "react";
+
+  function Note() {
+  return (
+  <div className="note">
+    <h1> Javascript and React.js </h1>
+    <p>I love to code Javascript and React.js</p>
+      </div>
+   );
+  }
+  export default Note;
+ 
